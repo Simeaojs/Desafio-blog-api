185fd97ed766000fe2bf8671e3bb6006683092e6
Simeao jose <simeaojs@gmail.com>
2023-10-05 20:05:11 -0300

    Demais Alterações

---
+++ src/main/java/com/blog/api/blogApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class blogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(blogApiApplication.class, args);
+	}
+
+}
