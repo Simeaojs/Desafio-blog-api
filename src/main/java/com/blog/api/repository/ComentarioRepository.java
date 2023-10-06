d94845199293f34a691c1b5d9cb3aab1a60aa341
Simeao jose <simeaojs@gmail.com>
2023-10-06 17:07:52 -0300

    Criando Query no ComentarioRepository

--- src/main/java/com/blog/api/repository/ComentarioRepository.java
+++ src/main/java/com/blog/api/repository/ComentarioRepository.java
@@ -1,7 +1,12 @@
 package com.blog.api.repository;
 
 import com.blog.api.model.Comentario;
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+
+    List<Comentario> findByPostIdPost(Long id);
+
 }
